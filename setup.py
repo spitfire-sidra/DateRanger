74dd1dbb6232e11afe7156bfab647b0a758881f6
spitfire-sidra <spitfire.sidra@gmail.com>
2015-01-19 15:31:49 +0800

    update setup.py and add a setup.cfg

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ from distutils.core import setup
 setup(
     name='DateRanger',
     url='https://github.com/spitfire-sidra/DateRanger',
-    version='0.2.0.dev',
+    version='0.2.1.dev',
     author='Amo Chen',
     author_email='spitfire.sidra@gmail.com',
     packages=['DateRanger',],
     license='License.txt',
-    description='Useful bussiness date ranges',
-    long_description=open('README.rst').read(),
+    description='Useful business date ranges'
 )
